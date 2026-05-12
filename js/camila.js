309a8a25b3c4ec2c5f8ab7d1e4b5ff21b9fe1d06
CAMILA ISABEL LUQUE CANO <94197634+Camila-Luque@users.noreply.github.com>
2026-05-12 04:33:37 +0000

    new

---
+++ js/camila.js
@@ -0,0 +1,15 @@
+{
+  "name": "CAMILA ISABEL LUQUE CANO",
+  "nickname": "Camila-Luque",
+  "github": "https://github.com/Camila-Luque",
+  "linkedin": "https://www.linkedin.com/in/camila-luque-3521b7346",
+  "instagram": "https://www.instagram.com/thisisbecamille/",
+  "image": "https://github.com/Camila-Luque.png",
+  "description": "Entusiasta de Inteligencia Artificial.",
+  "hobbies": [
+    "Asistir a charlas tech",
+    "Cantar",
+    "Aprender de tech  y minería",
+    "Competir"
+  
+}
